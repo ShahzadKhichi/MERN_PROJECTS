a7a8f27f113712b78a79e4f7cac504e13c50a02a
ShahzadKhichi <shahzadkhichi996@gmail.com>
2025-10-01 08:17:58 +0500

    all cors allowed

--- LMS_Site_Babbar/Backend/index.js
+++ LMS_Site_Babbar/Backend/index.js
@@ -13,7 +13,7 @@ const courseRouter = require("./src/Routes/course.routes");
 const profileRouter = require("./src/Routes/profile.routes");
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 app.use(express.json());
 app.use(cookieParser());
