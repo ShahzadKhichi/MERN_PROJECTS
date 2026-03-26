719ac7514b0d92a3b69b8a0d3c3811f19d07e045
ShahzadKhichi <shahzadkhichi996@gmail.com>
2026-03-26 18:38:31 +0500

    all problem crud working fine

--- leetcode-clone/problem-service/src/validators/problem.validator.ts
+++ leetcode-clone/problem-service/src/validators/problem.validator.ts
@@ -10,4 +10,16 @@ export const createProblemSchema = z.object({
         output: z.string().min(1),
     })).optional(),
 });
+
+export const updateProblemSchema = z.object({
+    title: z.string().min(1).optional(),
+    description: z.string().min(1).optional(),
+    difficulty: z.enum(["easy","medium","hard"]).optional(),
+    tags: z.array(z.string().min(1)).optional(),
+    testCases: z.array(z.object({
+        input: z.string().min(1),
+        output: z.string().min(1),
+    })).optional(),
+
+});
     
