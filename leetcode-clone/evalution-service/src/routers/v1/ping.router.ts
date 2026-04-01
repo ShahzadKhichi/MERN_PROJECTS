24de7adde599ffa65124bf6a1b5dfd422b3f9901
ShahzadKhichi <shahzadkhichi996@gmail.com>
2026-04-01 21:34:53 +0500

    health check added

--- leetcode-clone/evalution-service/src/routers/v1/ping.router.ts
+++ leetcode-clone/evalution-service/src/routers/v1/ping.router.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import { pingHandler } from '../../controllers/ping.controller';
 import {  validateRequestBody } from '../../validators';
 import { pingSchema } from '../../validators/ping.validator';
+import logger from '../../config/logger.config';
 
 const pingRouter = express.Router();
 
 pingRouter.get('/', validateRequestBody(pingSchema), pingHandler); // TODO: Resolve this TS compilation issue
 
 pingRouter.get('/health', (req, res) => {
+    logger.info("Health check")
     res.status(200).send('OK');
 });
 
